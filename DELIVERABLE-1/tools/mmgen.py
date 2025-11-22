252ed589f382c9925cc0d4caadd2b404d6f06550
Mirko Lana <lana.mirko@icloud.com>
2025-11-23 00:07:30 +0100

    feat: update `mmgen.py` CLI in order to generate matrices from `*.csv` files

--- DELIVERABLE-1/tools/mmgen.py
+++ DELIVERABLE-1/tools/mmgen.py
@@ -1,3 +1,7 @@
+import argparse
+import csv
+from typing import List, Tuple
+
 import scipy as sp
 from rich.prompt import Confirm, FloatPrompt, IntPrompt
 
@@ -31,25 +35,81 @@ def save_mm_matrix(matrix: sp.sparse.coo_matrix, filename: str) -> None:
     sp.io.mmwrite(filename, matrix)
 
 
+def make_parser() -> argparse.ArgumentParser:
+    """
+    Create an argument parser for the script.
+
+    Returns:
+    argparse.ArgumentParser: Configured argument parser.
+    """
+    parser = argparse.ArgumentParser(
+        description="Generate a random sparse matrix in Matrix Market format."
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        required=False,
+        help="Path to file that describes the matricies to generate.",
+    )
+    return parser
+
+
+def parse_csv_file_prompt(filename: str) -> List[dict]:
+    """
+    Parse a CSV file containing matrix specifications.
+
+    Parameters:
+    filename (str): Path to the CSV file.
+
+    Returns:
+    List[dict]: List of matrix specifications.
+    """
+    specs = []
+    with open(filename, "r") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            specs.append(row)
+    return specs
+
+
 def main() -> None:
-    is_running = True
+    """
+    Main function to run the script.
+    """
+    parser = make_parser()
+    args = parser.parse_args()
 
-    while is_running:
-        density = FloatPrompt.ask(
-            "Enter the density of the sparse matrix (0.01 <= density <= 1)", default=0.1
-        )
-        density = max(0.01, min(1.0, density))  # Clamp density between 0.01 and 1
+    if args.input:
+        matricies_to_generate = parse_csv_file_prompt(args.input)
+        for spec in matricies_to_generate:
+            print(spec)
+            m = int(spec["row"])
+            n = int(spec["col"])
+            density = float(spec["density"])
+            filename = spec.get(
+                "filename", f"mm_matrix_{m}x{n}_density{density:.2f}.mtx"
+            )
+            save_mm_matrix(generate_mm_matrix(m, n, density), filename)
 
-        m = IntPrompt.ask("Enter the number of rows", default="1000000")
-        n = IntPrompt.ask("Enter the number of columns", default="1000000")
-        filename = f"mm_matrix_{m}x{n}_density{density:.2f}.mtx"
+    else:
+        is_running = True
 
-        # Idk why, but  
-        save_mm_matrix(generate_mm_matrix(int(m), int(n), density), filename)
+        while is_running:
+            density = FloatPrompt.ask(
+                "Enter the density of the sparse matrix (0.01 <= density <= 1)",
+                default=0.1,
+            )
+            density = max(0.01, min(1.0, density))  # Clamp density between 0.01 and 1
 
-        is_running = Confirm.ask(
-            "Do you want to generate another matrix?", default=False
-        )
+            m = IntPrompt.ask("Enter the number of rows", default="1000000")
+            n = IntPrompt.ask("Enter the number of columns", default="1000000")
+            filename = f"mm_matrix_{m}x{n}_density{density:.2f}.mtx"
+            save_mm_matrix(generate_mm_matrix(int(m), int(n), density), filename)
+
+            is_running = Confirm.ask(
+                "Do you want to generate another matrix?", default=False
+            )
 
 
 if __name__ == "__main__":
